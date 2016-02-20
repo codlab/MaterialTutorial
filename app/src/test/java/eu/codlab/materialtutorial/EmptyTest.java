c27a563cb3410ee3485348fa57fa948595ab3bbb
Kevin Le Perf <codlabtech@gmail.com>
2016-02-20 23:09:34 +0100

    update test to add first support

--- app/src/test/java/eu/codlab/materialtutorial/EmptyTest.java
+++ app/src/test/java/eu/codlab/materialtutorial/EmptyTest.java
@@ -1,7 +1,15 @@
 package eu.codlab.materialtutorial;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by kevinleperf on 20/02/16.
  */
 public class EmptyTest {
+    @Test
+    public void assertEq() {
+        assertEquals(2, 1 + 1);
+    }
 }
