476a05104d500b929d253184e9b47e8d6d19dc3f
Kevin Le Perf <codlabtech@gmail.com>
2016-02-21 02:24:39 +0100

    fix wrong interface for example fragment

--- app/src/main/java/com/alexandrepiveteau/wallpaper/example/NotValidFragment.java
+++ app/src/main/java/com/alexandrepiveteau/wallpaper/example/NotValidFragment.java
@@ -1,5 +1,8 @@
 package com.alexandrepiveteau.wallpaper.example;
 
+import android.support.annotation.NonNull;
+
+import com.alexandrepiveteau.library.tutorial.ui.activities.TutorialActivity;
 import com.alexandrepiveteau.library.tutorial.ui.fragments.AbstractTutorialValidationFragment;
 
 /**
@@ -12,7 +15,7 @@ public class NotValidFragment extends AbstractTutorialValidationFragment {
     }
 
     @Override
-    public boolean onTryValidate() {
+    public boolean onTryValidate(@NonNull TutorialActivity parent) {
         return false;
     }
 }
